c5a2f18ae4707e7048c7ee9ca9b6654422f575cb
merima98 <merimaceranic98@gmail.com>
2020-08-25 13:09:32 +0200

    Adding middleware

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 
 const  express = require('express');
 
@@ -11,8 +10,7 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     console.log('In another middleware!');
+    res.send('<h1>Hello from Express.js</h1>')
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
