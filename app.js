f05eb12783f6b4b83cc4f6f75d9ab8323897a3ce
merima98 <merimaceranic98@gmail.com>
2020-08-25 13:19:44 +0200

    Handling different routes

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const  express = require('express');
 
 const  app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    next(); //Alows the request to continue to the next middleware in line
+app.use('/add-product',(req,res,next)=>{
+    console.log('In another middleware!');
+    res.send('<h1>The "Add product" Page</h1>')
 });
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log('In another middleware!');
     res.send('<h1>Hello from Express.js</h1>')
 });
