33a42976727cbd42766b7665e5dba969ae79c7b4
merima98 <merimaceranic98@gmail.com>
2020-08-25 16:36:27 +0200

    Limiting middleware execution to POST request

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/add-product',(req,res,next)=>{
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use('/product',(req, res, next)=>{
+app.post('/product',(req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
 });
