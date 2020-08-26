653804c912ea3cf1a4e331a32ae6ce4f61e0e451
merima98 <merimaceranic98@gmail.com>
2020-08-26 15:14:23 +0200

    Styling peges

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
+
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{ 
-    res.sendFile(path.join(__dirname,'..', 'views','shop.html'));
+    res.sendFile(path.join(rootDir, 'views','shop.html'));
 });
 
 module.exports= router;
