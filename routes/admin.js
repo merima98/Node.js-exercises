03c91efe3d1ac7e09d09db5baf8a6154d6ea82f8
merima98 <merimaceranic98@gmail.com>
2020-08-26 11:36:12 +0200

    Creating and serving HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product',(req,res,next)=>{ 
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(__dirname, '../','views','add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{ 
-    res.send('<h1>Hello from Express.js</h1>')
+    res.sendFile(path.join(__dirname,'../', 'views','shop.html'));
 });
 
 module.exports= router;
