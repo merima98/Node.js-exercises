8fe3d28d1d74af988f20f035fedd740a17f33e22
merima98 <merimaceranic98@gmail.com>
2020-08-26 11:49:02 +0200

    Returning a 404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 const  express = require('express');
 
@@ -15,7 +16,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next)=>{
 
-    res.status(404).send('<h1>Page not found!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3500);
