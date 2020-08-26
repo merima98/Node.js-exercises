a6a50cb216b7143d06d1a9ee45172504d1e81119
merima98 <merimaceranic98@gmail.com>
2020-08-26 10:44:40 +0200

    Adding a 404 error page

--- app.js
+++ app.js
@@ -13,4 +13,9 @@ app.use(bodyParser.urlencoded({extended: true})); //running a version of Express
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next)=>{
+
+    res.status(404).send('<h1>Page not found!</h1>');
+});
+
 app.listen(3500);
