c6963758229e567aa5f8290cc3e7dd36de44864f
merima98 <merimaceranic98@gmail.com>
2020-08-26 12:21:16 +0200

    Using a helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product',(req,res,next)=>{ 
-    res.sendFile(path.join(__dirname, '../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{ 
-    res.sendFile(path.join(__dirname,'../', 'views','shop.html'));
+    res.sendFile(path.join(__dirname,'..', 'views','shop.html'));
 });
 
 module.exports= router;
