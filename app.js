b889bc9775953bad4dae1df739680a9e7a827715
merima98 <merimaceranic98@gmail.com>
2020-08-27 16:13:08 +0200

    Serbing files statically

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true})); //running a version of Express that is 4.16+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
