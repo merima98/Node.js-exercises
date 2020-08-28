303bfc9a54862ffb6526cfd35ea5baa53e6951a4
merima98 <merimaceranic98@gmail.com>
2020-08-28 15:23:41 +0200

    Converting HTML files to Pug

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next)=>{
 
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 });
 
 app.listen(3500);
