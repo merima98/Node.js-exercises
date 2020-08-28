11338624355b3d04128102a44c0afa04189ecdc8
merima98 <merimaceranic98@gmail.com>
2020-08-28 14:36:50 +0200

    Outputing dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{ 
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop',{prods: products, docTitle: 'Shop'});
 });
 
 module.exports= router;
