304cff83a6e1a0cc79408c4becd11340b5def665
merima98 <merimaceranic98@gmail.com>
2020-08-30 21:27:57 +0200

    Converting project to handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.get('/',(req,res,next)=>{ 
   const products = adminData.products;
-  res.render('shop',{prods: products, pageTitle: 'Shop', path: '/'});
+  res.render('shop',{prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
 });
 
 module.exports= router;
