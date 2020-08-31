3a2868dd9561d2f3e4e6f42c57addaafb5d5cd83
merima98 <merimaceranic98@gmail.com>
2020-08-31 15:23:15 +0200

    Adding ejs template to the project

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+ 
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
