5f8a09284add461a67a1ffd051e5d015e741bdc8
merima98 <merimaceranic98@gmail.com>
2020-09-01 12:05:06 +0200

    Adding Modified controller for error

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
- 
+
+
+
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views',__dirname+ '/views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { executionAsyncResource } = require('async_hooks');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//dodajem
+app.use(express.static('views'));
+//kraj dodavanja
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
-
+app.use(errorController.get404);
 
 app.listen(3500);
