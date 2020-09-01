51a0b5dbc58dc130aa4adf29f08367b08f176d37
merima98 <merimaceranic98@gmail.com>
2020-09-02 01:03:43 +0200

    Adding orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -31,6 +31,13 @@ exports.getCart=(req, res, next)  =>{
     });
 };
 
+exports.getOrders=(req, res, next)  =>{
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
+};
+
 exports.getCheckout=(req, res, next)  =>{
     res.render('shop/checkout',{
         path: '/checkout',
