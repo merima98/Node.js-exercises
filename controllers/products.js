a7ea2b846f52c08e444603c1714b6839e146f64d
merima98 <merimaceranic98@gmail.com>
2020-09-01 12:34:26 +0200

    Adding a product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req,res,next)=>{ 
     res.render(
@@ -13,11 +13,13 @@ exports.getAddProduct = (req,res,next)=>{
 };
 
 exports.postAddProduct = (req, res, next)=>{
-    products.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req,res,next)=>{ 
+    const products = Product.fetchAll();
     res.render('shop',{
       prods: products, 
       pageTitle: 'Shop', 
