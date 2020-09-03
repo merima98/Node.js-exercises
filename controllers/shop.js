d05fbe76873a3bf0890c628e103b5988b085e31f
merima98 <merimaceranic98@gmail.com>
2020-09-03 16:25:36 +0200

    Rendering the product detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,9 +16,14 @@ exports.getProducts = (req,res,next)=>{
 exports.getProduct= (req, res, next)=>{
     const prodId = req.params.productId;
     Product.findById(prodId, product=>{
-        console.log(product);
+        res.render(
+            'shop/product-detail',
+             {
+                 product: product,
+                  pageTitle: product.title,
+                  path: '/products'
+            });
     } );
-    res.redirect('/');
 };
 
 exports.getIndex=(req, res, next)  =>{
