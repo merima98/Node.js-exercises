151c387acc3f88564b9db262b40c788be811a41d
merima98 <merimaceranic98@gmail.com>
2020-09-03 19:22:18 +0200

    Pre-populating the edit product page with data

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,8 @@ exports.getAddProduct = (req,res,next)=>{
         'admin/edit-product', 
         {
             pageTitle: 'Add Product',
-            path: '/admin/add-product'
+            path: '/admin/add-product',
+            editing: false
         });
 };
 
@@ -25,13 +26,21 @@ exports.getEditProduct = (req,res,next)=>{
     if(!editMode){
         res.redirect('/');
     }
-    res.render(
-        'admin/edit-product', 
-        {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode
-        });
+    const prodId = req.params.productId;
+    Product.findById(prodId, product=>{
+        if(!product){
+            return res.redirect('/');
+        }
+        res.render(
+            'admin/edit-product', 
+            {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+    });
+  
 };
 
 exports.getProducts = (req, res, next)=>{
