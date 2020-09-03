1a1a5b057e8c5f8a414916aab6d362021afea5bc
merima98 <merimaceranic98@gmail.com>
2020-09-03 15:56:55 +0200

    Extracting dynamic params

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,12 @@ exports.getProducts = (req,res,next)=>{
     });
   };
 
+exports.getProduct= (req, res, next)=>{
+    const prodId = req.params.productId;
+    console.log(prodId);
+    res.redirect('/');
+};
+
 exports.getIndex=(req, res, next)  =>{
     Product.fetchAll((products)=>{
         res.render('shop/index',{
--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,8 @@ router.get('/',shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
