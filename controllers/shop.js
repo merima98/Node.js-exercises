482cd2367dcce6208a719120fe9dba9835a23a76
merima98 <merimaceranic98@gmail.com>
2020-09-03 16:45:14 +0200

    Passing data with post request

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,6 +44,12 @@ exports.getCart=(req, res, next)  =>{
     });
 };
 
+exports.postCart = (req, res, next)=>{
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+};
+
 exports.getOrders=(req, res, next)  =>{
     res.render('shop/orders', {
         path: '/orders',
