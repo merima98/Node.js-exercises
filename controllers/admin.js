8dc39d55671a4392308cfdcd3c4e0f568f7a9184
merima98 <merimaceranic98@gmail.com>
2020-09-04 14:26:39 +0200

    Linking the edit page

--- controllers/admin.js
+++ controllers/admin.js
@@ -43,6 +43,10 @@ exports.getEditProduct = (req,res,next)=>{
   
 };
 
+exports.postEditProducts=(req, res, next)=>{
+
+};
+
 exports.getProducts = (req, res, next)=>{
     Product.fetchAll((products)=>{
         res.render('admin/products',{
--- routes/admin.js
+++ routes/admin.js
@@ -20,4 +20,6 @@ router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId',adminController.getEditProduct);
 
+router.post('/edit-product');
+
 module.exports = router;
