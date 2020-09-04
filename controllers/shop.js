3e29fb4f6288b2fb09cecb08330b479203e34a60
merima98 <merimaceranic98@gmail.com>
2020-09-04 16:37:50 +0200

    Deleting cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,6 +65,14 @@ exports.postCart = (req, res, next)=>{
     res.redirect('/cart');
 };
 
+exports.postCartDeleteProduct = (req, res, next)  =>{
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price)
+        res.redirect('/cart');
+    });
+};
+
 exports.getOrders=(req, res, next)  =>{
     res.render('shop/orders', {
         path: '/orders',
--- routes/shop.js
+++ routes/shop.js
@@ -17,6 +17,8 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
+router.post('/cart-delete-item',shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
