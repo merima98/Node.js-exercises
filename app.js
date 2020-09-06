ba37882d3dc890cea6ed346aa3a3691307e99363
merima98 <merimaceranic98@gmail.com>
2020-09-06 18:58:13 +0200

    Syncing JS definitions to the database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 
 const app = express();
@@ -35,4 +35,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3500);
+
+sequelize
+    .sync()
+    .then(result=>{
+    //console.log(result);
+    app.listen(3500);
+    })
+    .catch(err=>console.log(err));
+
--- models/product.js
+++ models/product.js
@@ -1,34 +1,27 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../util/database');
 
-
-
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product',{
+  id: {
+    type: Sequelize.INTEGER, 
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type : Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-   return db.execute('INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)',
-      [this.title, this.price, this.description, this.imageUrl]
-      );
-  }
-
-  static deleteById(id) {
-     
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product;
