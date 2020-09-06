69a12b8d12d4b4fefcd45daf33b0555ef8f04bfc
merima98 <merimaceranic98@gmail.com>
2020-09-06 16:22:41 +0200

    Fetching products

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,14 +3,16 @@ const Cart = require('../models/cart');
 
 
 exports.getProducts = (req,res,next)=>{ 
-    Product.fetchAll((products)=>{
+    Product.fetchAll()
+    .then(([rows,fieldData])=> {
         res.render('shop/product-list',{
-            prods: products, 
+            prods: rows, 
             pageTitle: 'All Products', 
             path: '/products', 
             layout: 'main-layout'
           });
-    });
+    })
+    .catch(err=> {console.log(err);});
   };
 
 exports.getProduct= (req, res, next)=>{
