760d5cc346ce488de1315b8026af952e8b331bee
merima98 <merimaceranic98@gmail.com>
2020-09-06 16:40:39 +0200

    Fetching a single product with the where condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,15 +17,17 @@ exports.getProducts = (req,res,next)=>{
 
 exports.getProduct= (req, res, next)=>{
     const prodId = req.params.productId;
-    Product.findById(prodId, product=>{
+    Product.findById(prodId)
+    .then(([product])=>{
         res.render(
             'shop/product-detail',
              {
-                 product: product,
+                 product: product[0],
                   pageTitle: product.title,
                   path: '/products'
             });
-    } );
+    })
+    .catch(err=>console.log(err));
 };
 
 exports.getIndex=(req, res, next)  =>{
--- models/product.js
+++ models/product.js
@@ -29,6 +29,6 @@ module.exports = class Product {
   }
 
   static findById(id) {
-   
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
