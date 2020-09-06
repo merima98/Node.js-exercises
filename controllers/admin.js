8943a7aa0b483fa997924e4eb46d3b6feb9f78c4
merima98 <merimaceranic98@gmail.com>
2020-09-06 16:33:14 +0200

    Inserting data into the database

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,8 +16,12 @@ exports.postAddProduct = (req, res, next)=>{
     const price = req.body.price;
     const description = req.body.description;
     const product = new Product(null,title, imageUrl, description, price);
-    product.save();
-    res.redirect('/');
+    product
+    .save()
+    .then(()=>{
+        res.redirect('/');
+    })
+    .catch(err=>console.log(err));
 }; 
 
 
--- models/product.js
+++ models/product.js
@@ -15,7 +15,9 @@ module.exports = class Product {
   }
 
   save() {
-    
+   return db.execute('INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)',
+      [this.title, this.price, this.description, this.imageUrl]
+      );
   }
 
   static deleteById(id) {
