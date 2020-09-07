ea3b8dae4ad8207dcdeb77eaf4bbb2bc2f815847
merima98 <merimaceranic98@gmail.com>
2020-09-07 14:45:14 +0200

    Adding existing products and retrieving cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,17 +65,43 @@ exports.getCart=(req, res, next)  =>{
     .catch(err=> console.log(err));
 };
 
-exports.postCart = (req, res, next)=>{
+exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, (product)=>{
-    Cart.addProduct(prodId, product.price);
-    })
-    res.redirect('/cart');
-};
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user
+      .getCart()
+      .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts({ where: { id: prodId } });
+      })
+      .then(products => {
+        let product;
+        if (products.length > 0) {
+          product = products[0];
+        }
+  
+        if (product) {
+          const oldQuantity = product.cartItem.quantity;
+          newQuantity = oldQuantity + 1;
+          return product;
+        }
+        return Product.findByPk(prodId);
+      })
+      .then(product => {
+        return fetchedCart.addProduct(product, {
+          through: { quantity: newQuantity }
+        });
+      })
+      .then(() => {
+        res.redirect('/cart');
+      })
+      .catch(err => console.log(err));
+  };
 
 exports.postCartDeleteProduct = (req, res, next)  =>{
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
+    Product.findByPk(prodId, product => {
         Cart.deleteProduct(prodId, product.price)
         res.redirect('/cart');
     });
