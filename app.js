4fdefeddf233037448036c140586b31bad1f6151
merima98 <merimaceranic98@gmail.com>
2020-09-07 12:10:17 +0200

    Adding a one to many relationship

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 
 const app = express();
@@ -36,8 +38,11 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
+Product.belongsTo(User, {constraints: true, onDelete:'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-    .sync()
+    .sync({force: true})
     .then(result=>{ 
     //console.log(result);
     app.listen(3500);
