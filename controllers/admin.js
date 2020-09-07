817f8e5c4c0bf10a0bdc87f3ecd7ec4c01813f0d
merima98 <merimaceranic98@gmail.com>
2020-09-07 12:36:06 +0200

    Using association methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res, next)=>{
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    Product.create({
+    req.user.createProduct({
         title: title,
         price: price,
         imageUrl: imageUrl,
