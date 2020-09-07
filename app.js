1da33518be9944260a400ab9119349e6a49d2a42
merima98 <merimaceranic98@gmail.com>
2020-09-07 13:37:06 +0200

    Adding one to many and many to many relations

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart= require('./models/cart');
+const CartItem= require('./models/cart-item');
 
 
 const app = express();
@@ -52,6 +54,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete:'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+
+
+
 
 sequelize
     // .sync({force: true})
