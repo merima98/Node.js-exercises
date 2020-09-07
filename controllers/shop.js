8680f6502913634756026c194ae7a43820b17972
merima98 <merimaceranic98@gmail.com>
2020-09-07 15:40:47 +0200

    Storing Cartitems as Orderitems

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 
 exports.getProducts = (req,res,next)=>{ 
@@ -115,6 +114,31 @@ exports.postCartDeleteProduct = (req, res, next)  =>{
     })
     .catch(err=> console.log(err)); 
 };
+
+exports.postOrder= (req,res,next) =>{
+
+    req.user
+    .getCart()
+    .then(cart=>{
+        return cart.getProducts();
+    })
+    .then(products=>{
+        return req.user.createOrder()
+        .then(order=>{
+            return order.addProducts(products.map(product=>{
+               product.orderItem = {quantity: product.cartItem.quantity};
+                return product;
+            }));
+        })
+        .catch(err=>console.log(err));
+    })
+    .then(result=>{
+        res.redirect('/orders');
+    })
+    .catch(err=> console.log(err));
+
+};
+
 
 exports.getOrders=(req, res, next)  =>{
     res.render('shop/orders', {
--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const { route } = require('./admin');
 
 
 const router = express.Router();
@@ -18,6 +19,8 @@ router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item',shopController.postCartDeleteProduct);
+
+router.post('/create-order', shopController.postOrder);
 
 router.get('/orders', shopController.getOrders);
 
