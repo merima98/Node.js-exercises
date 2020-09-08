1b629210de70bb180b87131b6d061337f19961c1
merima98 <merimaceranic98@gmail.com>
2020-09-08 23:37:16 +0200

    Getting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -94,7 +94,7 @@ exports.postOrder= (req,res,next) =>{
 
 exports.getOrders=(req, res, next)  =>{
     req.user
-        .getOrders({include: ['products']})
+        .getOrders()
         .then(orders=>{ 
             res.render('shop/orders', {
                 path: '/orders',
--- models/user.js
+++ models/user.js
@@ -102,6 +102,7 @@ class User {
 
     getOrders(){
        const db = getDb();
+       return db.collection('orders').find({'user._id': new ObjectId(this._id)}).toArray();
     }
 
     static findById(userId){
