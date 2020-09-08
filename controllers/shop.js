881501be6f2e44c3fa86e5ddb57e09b27eb32a8e
merima98 <merimaceranic98@gmail.com>
2020-09-08 23:30:43 +0200

    Adding an order and adding relational order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -83,36 +83,14 @@ exports.postCartDeleteProduct = (req, res, next)  =>{
 };
 
 exports.postOrder= (req,res,next) =>{
-
     let fetchedCart;
-
-
     req.user
-    .getCart()
-    .then(cart=>{
-        fetchedCart = cart;
-        return cart.getProducts();
-    })
-    .then(products=>{
-        return req.user.createOrder()
-        .then(order=>{
-            return order.addProducts(products.map(product=>{
-               product.orderItem = {quantity: product.cartItem.quantity};
-                return product;
-            }));
-        })
-        .catch(err=>console.log(err));
-    })
-    .then(result=>{
-        return fetchedCart.setProducts(null); 
-    })
-    .then(result=>{
+      .addOrder()
+      .then((result) => {
         res.redirect('/orders');
-    })
-    .catch(err=> console.log(err));
-
+      })
+      .catch(err => console.log(err));
 };
-
 
 exports.getOrders=(req, res, next)  =>{
     req.user
--- models/user.js
+++ models/user.js
@@ -76,6 +76,33 @@ class User {
        );
     }
 
+    addOrder() {
+        const db = getDb();
+        return this.getCart()
+          .then(products => {
+            const order = {
+              items: products,
+              user: {
+                _id: new ObjectId(this._id),
+                name: this.name
+              }
+            };
+            return db.collection('orders').insertOne(order);
+          })
+          .then(result => {
+            this.cart = { items: [] };
+            return db
+              .collection('users')
+              .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: { items: [] } } }
+              );
+          });
+      }
+
+    getOrders(){
+       const db = getDb();
+    }
 
     static findById(userId){
         const db = getDb();
