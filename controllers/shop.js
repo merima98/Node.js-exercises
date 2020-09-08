ca0d8ae99e35a06695de65897ec3e5d6acb12d11
merima98 <merimaceranic98@gmail.com>
2020-09-08 22:43:51 +0200

    Displaying the cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -50,16 +50,12 @@ exports.getIndex=(req, res, next)  =>{
 exports.getCart=(req, res, next)  =>{
 
     req.user.getCart()
-    .then(cart=>{
-        return cart.getProducts()
-        .then(products=>{
-            res.render('shop/cart', {
-                path: '/cart',
-                pageTitle: 'Your Cart',
-                products: products
-            });
-        })
-        .catch(err=> console.log(err));
+    .then(products=>{
+        res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+        });
     })
     .catch(err=> console.log(err));
 };
@@ -72,6 +68,7 @@ exports.postCart = (req, res, next) => {
       })
       .then(result => {
         console.log(result);
+        res.redirect('/cart');
       });
 
     // let fetchedCart;
--- models/user.js
+++ models/user.js
@@ -45,6 +45,25 @@ class User {
         );
       }
 
+    getCart(){
+        const db = getDb();
+        const productIds = this.cart.items.map(i=>{
+            return i.productId;
+        });
+        return db
+            .collection('products')
+            .find({_id:{$in: productIds}})
+            .toArray()
+            .then(products=>{
+               return products.map(p=>{
+                   return {...p, quantity: this.cart.items.find(i=>{
+                       return i.productId.toString() === p._id.toString();
+                   }).quantity
+                };
+               }); 
+            });
+    }  
+
     static findById(userId){
         const db = getDb();
         return db
