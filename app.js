b33d9e6751c5af87712d0c261a368a1afde0a6a5
merima98 <merimaceranic98@gmail.com>
2020-09-08 13:52:00 +0200

    Installing the MongoDB driver

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart= require('./models/cart');
-const CartItem= require('./models/cart-item');
-const Order= require('./models/order');
-const OrderItem= require('./models/order-item');
+const mongoConnect = require('./util/database');
 
 
 const app = express();
@@ -21,25 +15,28 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views',__dirname+ '/views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+
+//  TO DO AFTER
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 
 
 
 const { executionAsyncResource } = require('async_hooks');
-const { use } = require('./routes/shop');
+//  TO DO AFTER
+//const { use } = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next)=>{
-    User.findByPk(1)
-    .then(user=>{
-        req.user=user;
-        next();
-    })
-    .catch(err=>console.log(err));
+    // User.findByPk(1)
+    // .then(user=>{
+    //     req.user=user;
+    //     next();
+    // })
+    // .catch(err=>console.log(err));
 });
 
 
@@ -48,44 +45,16 @@ app.use((req,res,next)=>{
 app.use(express.static('views'));
 //kraj dodavanja
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+//  TO DO AFTER
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-Product.belongsTo(User, {constraints: true, onDelete:'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product,{through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem });
+mongoConnect((client)=>{
+    console.log(client);
+    app.listen(3500);
+});
 
 
-
-
-sequelize
-    // .sync({force: true})
-    .sync()
-    .then(result=>{ 
-        return User.findByPk(1);
-    //console.log(result);
-    })
-    .then(user=>{
-        if(!user){
-           return  User.create({name: 'Merima', email: 'test@gmail.com'});
-        }
-        return user;
-    })
-    .then(user=>{
-        // console.log(user);
-        return user.createCart();
-    })
-    .then(cart=>{
-        app.listen(3500);
-    })
-    .catch(err=>console.log(err));
 
--- util/database.js
+++ util/database.js
@@ -1,9 +1,21 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb') ;
 
-const sequelize = new Sequelize('node-complete','root','merima1998' ,{
-    dialect: 'mysql', 
-    host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = (callback) =>{
+
+    MongoClient.connect(
+        'mongodb+srv://merima98:merima1998@cluster0.w4ehk.mongodb.net/<dbname>?retryWrites=true&w=majority'
+        )
+        .then(client=>{
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch(err=> console.log(err));
+};
+
+module.exports = mongoConnect;
+
+
+
 
