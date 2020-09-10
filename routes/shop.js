247795bcc82f50ddcd0dfb150c74527ec3e385fd
merima98 <merimaceranic98@gmail.com>
2020-09-10 22:32:02 +0200

    Fetching a single product through Mongoose

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/',shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
-// router.get('/products/:productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
 // router.get('/cart', shopController.getCart);
 
