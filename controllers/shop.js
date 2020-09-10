a5dbbcad56cc9a8ef074650485bab0e1fe150f2a
merima98 <merimaceranic98@gmail.com>
2020-09-11 00:36:07 +0200

    Getting and displaying the orders in Mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -115,9 +115,8 @@ exports.postOrder= (req,res,next) =>{
 };
 
 exports.getOrders=(req, res, next)  =>{
-    req.user
-        .getOrders()
-        .then(orders=>{ 
+    Order.find({"user.userId": req.user._id})
+    .then(orders=>{ 
             res.render('shop/orders', {
                 path: '/orders',
                 pageTitle: 'Your Orders',
