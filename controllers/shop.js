739cc2bab04378cb5673b014d995afdf4f5333ad
merima98 <merimaceranic98@gmail.com>
2020-09-10 23:50:12 +0200

    Loading the cart in Mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -50,9 +50,11 @@ exports.getIndex=(req, res, next)  =>{
 };
 
 exports.getCart=(req, res, next)  =>{
-
-    req.user.getCart()
-    .then(products=>{
+    req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user=>{
+        const products=user.cart.items;
         res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,7 @@ router.get('/products',shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-// router.get('/cart', shopController.getCart);
+router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
