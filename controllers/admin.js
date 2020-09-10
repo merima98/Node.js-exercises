9da489d04e19ebdd7e2bcec295cdc6bce5efa8a8
merima98 <merimaceranic98@gmail.com>
2020-09-10 22:45:53 +0200

    Deleting products through Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -98,7 +98,7 @@ exports.getProducts = (req, res, next)=>{
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-     Product.deleteById(prodId)
+     Product.findByIdAndRemove(prodId)
       .then(() => {
         console.log('DESTROYED PRODUCT');
         res.redirect('/admin/products');
