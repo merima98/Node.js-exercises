6f800e8da5b9b2d03d4670f18b5bcb0feedaec93
merima98 <merimaceranic98@gmail.com>
2020-09-22 10:50:40 +0200

    Senitizing data

--- routes/auth.js
+++ routes/auth.js
@@ -12,10 +12,12 @@ router.get('/login', authController.getLogin);
 router.post('/login', [
     body('email')
         .isEmail()
-        .withMessage('Please enter a valid email address!'),
+        .withMessage('Please enter a valid email address!')
+        .normalizeEmail(),
     body('password', 'Password has to be valid! Min number of characters is 5!')
         .isLength({ min: 5 })
         .isAlphanumeric()
+        .trim()
 ], authController.postLogin);
 
 router.post('/logout', authController.postLogout);
@@ -35,13 +37,16 @@ router.post(
                             return Promise.reject('E-Mail exists already! Please pick a different one.');
                         }
                     });
-            }),
+            })
+            .normalizeEmail(),
         body('password',
             'Please enter a password with only numbers and text and at least 5 characters!'
         )
             .isLength({ min: 5 })
-            .isAlphanumeric(),
+            .isAlphanumeric()
+            .trim(),
         body('confirmPassword')
+            .trim()
             .custom((value, { req }) => {
                 if (value !== req.body.password) {
                     throw new Error('Passwords have to mach!');
