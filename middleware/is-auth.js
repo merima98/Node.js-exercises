2ef934d4597b5f154a4380c5ac9882d0a84841ab
merima98 <merimaceranic98@gmail.com>
2020-09-22 18:22:13 +0200

    Using the error handling middleware correctly

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,5 @@
-module.exports = (req, res, next)=>{
-    if(!req.session.isLoggedIn){
+module.exports = (req, res, next) => {
+    if (!req.session.isLoggedIn) {
         return res.redirect('/login');
     }
     next();
