dd09126527bbeeeefc1291aa2ad365bb5df6bbe8
merima98 <merimaceranic98@gmail.com>
2020-09-23 10:52:36 +0200

    Serving images statically

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(
     multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
     session({
         secret: 'my secret',
