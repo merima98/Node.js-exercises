e51aed06a92f41cf43830adac5efde89bb5acc67
merima98 <merimaceranic98@gmail.com>
2020-09-23 13:43:49 +0200

    Using PDFKit for .pdf Generation

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -162,6 +163,18 @@ exports.getInvoice = (req, res, next) => {
             }
             const invoiceName = 'invoice-' + orderId + '.pdf';
             const invoicePath = path.join('data', 'invoices', invoiceName);
+
+            const pdfDoc = new PDFDocument();
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader(
+                'Content-Disposition',
+                'inline; filename="' + invoiceName + '"'
+            );
+            pdfDoc.pipe(fs.createWriteStream(invoicePath));
+            pdfDoc.pipe(res);
+
+            pdfDoc.text('Hello world!');
+            pdfDoc.end();
             // fs.readFile(invoicePath, (err, data) => {
             //     if (err) {
             //         return next(err);
@@ -174,13 +187,9 @@ exports.getInvoice = (req, res, next) => {
             //     res.send(data);
             // });
 
-            const file = fs.createReadStream(invoicePath);
-            res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader(
-                'Content-Disposition',
-                'inline; filename="' + invoiceName + '"'
-            );
-            file.pipe(res);
+            // const file = fs.createReadStream(invoicePath);
+
+            // file.pipe(res);
         })
         .catch(err => next(err));
 };
