fb3337cbacab45e381156fde71667bbe62a5db80
merima98 <merimaceranic98@gmail.com>
2020-09-23 21:43:01 +0200

    Re-using the pagination logic & controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,13 +8,26 @@ const Order = require('../models/order');
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
     Product.find()
-        .then(products => {
-            console.log(products);
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        }).then(products => {
             res.render('shop/product-list', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
