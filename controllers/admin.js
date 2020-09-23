2fc1fe2b02e04ee559ed796566ac81dd9cecc363
merima98 <merimaceranic98@gmail.com>
2020-09-23 10:36:10 +0200

    Storing file data in the database

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,10 +18,27 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.file;
+    const image = req.file;
     const price = req.body.price;
     const description = req.body.description;
-    console.log(imageUrl);
+
+    if (!image) { //if it is not set
+        return res.status(422).render(
+            'admin/edit-product',
+            {
+                pageTitle: 'Add Product',
+                path: '/admin/add-product',
+                editing: false,
+                hasError: true,
+                product: {
+                    title: title,
+                    price: price,
+                    description: description
+                },
+                errorMessage: 'Attached file is not an image!',
+                validationErrors: []
+            });
+    }
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -44,6 +61,7 @@ exports.postAddProduct = (req, res, next) => {
             });
     }
 
+    const imageUrl = image.path;
     const product = new Product({
         title: title,
         price: price,
@@ -100,7 +118,7 @@ exports.postEditProducts = (req, res, next) => {
     const prodId = req.body.productId;
     const updatetTitle = req.body.title;
     const updatetPrice = req.body.price;
-    const updatetImage = req.body.imageUrl;
+    const image = req.file;
     const updatetDesc = req.body.description;
 
     const errors = validationResult(req);
@@ -115,7 +133,6 @@ exports.postEditProducts = (req, res, next) => {
                 hasError: true,
                 product: {
                     title: updatetTitle,
-                    imageUrl: updatetImage,
                     price: updatetPrice,
                     description: updatetDesc,
                     _id: prodId
@@ -132,7 +149,9 @@ exports.postEditProducts = (req, res, next) => {
         }
         product.title = updatetTitle;
         product.price = updatetPrice;
-        product.imageUrl = updatetImage;
+        if (image) {
+            product.imageUrl = image.path;
+        }
         product.description = updatetDesc;
         return product.save()
             .then(result => {
--- routes/admin.js
+++ routes/admin.js
@@ -37,8 +37,6 @@ router.post('/edit-product',
             .isString()
             .isLength({ min: 3 })
             .trim(),
-        body('imageUrl')
-            .isURL(),
         body('price')
             .isFloat(),
         body('description')
