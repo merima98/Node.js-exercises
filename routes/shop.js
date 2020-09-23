e0d32d5496d80af7ac3836c8bca0bd368ab2cc2a
merima98 <merimaceranic98@gmail.com>
2020-09-23 13:06:28 +0200

    Downloading files with authentication | setting file type headers | Restricking file access

--- routes/shop.js
+++ routes/shop.js
@@ -5,25 +5,24 @@ const express = require('express');
 const shopController = require('../controllers/shop');
 const isAuth = require('../middleware/is-auth');
 
-const { route } = require('./admin');
-
-
 const router = express.Router();
 
-router.get('/',shopController.getIndex);
+router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart',isAuth, shopController.getCart);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.post('/cart',isAuth, shopController.postCart);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/cart-delete-item',isAuth,shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.post('/create-order',isAuth, shopController.postOrder);
+router.post('/create-order', isAuth, shopController.postOrder);
 
-router.get('/orders',isAuth, shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 
-module.exports= router;
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+
+module.exports = router;
