aff0d53ebff58afb51d2b04fa1b0e156fdcb4b06
merima98 <merimaceranic98@gmail.com>
2020-09-23 14:12:37 +0200

    Genereting .pdf files with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -173,7 +173,21 @@ exports.getInvoice = (req, res, next) => {
             pdfDoc.pipe(fs.createWriteStream(invoicePath));
             pdfDoc.pipe(res);
 
-            pdfDoc.text('Hello world!');
+            pdfDoc.fontSize(26).text('Invoice', {
+                underline: true
+            });
+            pdfDoc.text('----------------------------------------------------');
+
+            let totalPrice = 0;
+            order.products.forEach(prod => {
+                totalPrice += prod.quantity * prod.product.price;
+                pdfDoc.fontSize(14).text(prod.product.title + ' - ' + prod.quantity + 'x ' + ' $' + prod.product.price);
+            });
+            pdfDoc.text('---------------------------');
+
+            pdfDoc.fontSize(20).text('Total price: $' + totalPrice);
+
+
             pdfDoc.end();
             // fs.readFile(invoicePath, (err, data) => {
             //     if (err) {
