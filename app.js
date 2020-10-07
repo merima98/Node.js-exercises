97b6a8fb0339006846db0bdd933ab2ebe1de0f3d
merima98 <merimaceranic98@gmail.com>
2020-10-07 18:34:19 +0200

    Prepared for deployment

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
